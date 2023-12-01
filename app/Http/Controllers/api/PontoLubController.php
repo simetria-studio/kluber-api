94c403d42d4a0a006c344993c78df6b5ae585edc
Felipe Cruz <fecruz2022@gmail.com>
2023-12-01 15:47:55 -0300

    get api

--- app/Http/Controllers/api/PontoLubController.php
+++ app/Http/Controllers/api/PontoLubController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Component;
+use App\Models\ProdutoLub;
+use App\Models\UnidadeMed;
 use Illuminate\Http\Request;
 
 class PontoLubController extends Controller
@@ -10,15 +13,7 @@ class PontoLubController extends Controller
     public function components()
     {
 
-        $components = [
-            [
-                'id' => 1,
-                'name' => 'ELOS',
-                'description' => 'Esse é o componente 1'
-            ],
-
-
-        ];
+        $components = Component::where('nome', 'componente')->get();
 
         return response()->json($components);
     }
@@ -26,18 +21,7 @@ class PontoLubController extends Controller
     public function condOp()
     {
 
-        $condOp = [
-            [
-                'id' => 1,
-                'name' => 'OPERANDO',
-                'description' => 'Esse é o componente 1'
-            ],
-            [
-                'id' => 2,
-                'name' => 'NÂO OPERANDO',
-                'description' => 'Esse é o componente 1'
-            ],
-        ];
+        $condOp = Component::where('nome', 'condicao_operacional')->get();
 
         return response()->json($condOp);
     }
@@ -45,19 +29,7 @@ class PontoLubController extends Controller
     public function atividadeBreve()
     {
 
-        $atividadeBreve = [
-            [
-                'id' => 1,
-                'name' => 'REALIZAR LIMPEZA E LUBRIFICAR',
-                'description' => 'Esse é o componente 1'
-            ],
-            [
-                'id' => 2,
-                'name' => 'OUTROS',
-                'description' => 'Esse é o componente 1'
-            ],
-
-        ];
+        $atividadeBreve = Component::where('nome', 'ativade_breve')->get();
 
         return response()->json($atividadeBreve);
     }
@@ -65,18 +37,7 @@ class PontoLubController extends Controller
     public function unidadeMed()
     {
 
-        $unidadeMed = [
-            [
-                'id' => 1,
-                'name' => 'UN',
-                'description' => 'Esse é o componente 1'
-            ],
-            [
-                'id' => 2,
-                'name' => 'LT',
-                'description' => 'Esse é o componente 1'
-            ],
-        ];
+        $unidadeMed = UnidadeMed::all();
 
         return response()->json($unidadeMed);
     }
@@ -84,114 +45,21 @@ class PontoLubController extends Controller
     public function frequencia()
     {
 
-        $frequencia = [
-            [
-                'id' => 1,
-                'name' => 'DIÁRIO',
-                'description' => 'Esse é o componente 1'
-            ],
-            [
-                'id' => 2,
-                'name' => 'SEMANAL',
-                'description' => 'Esse é o componente 1'
-            ],
-            [
-                'id' => 3,
-                'name' => 'QUINZENAL',
-                'description' => 'Esse é o componente 1'
-            ],
-            [
-                'id' => 4,
-                'name' => 'MENSAL',
-                'description' => 'Esse é o componente 1'
-            ],
-            [
-                'id' => 5,
-                'name' => 'BIMESTRAL',
-                'description' => 'Esse é o componente 1'
-            ],
-            [
-                'id' => 6,
-                'name' => 'TRIMESTRAL',
-                'description' => 'Esse é o componente 1'
-            ],
-            [
-                'id' => 7,
-                'name' => 'SEMESTRAL',
-                'description' => 'Esse é o componente 1'
-            ],
-            [
-                'id' => 8,
-                'name' => 'ANUAL',
-                'description' => 'Esse é o componente 1'
-            ],
-            [
-                'id' => 9,
-                'name' => 'BIENAL',
-                'description' => 'Esse é o componente 1'
-            ],
-            [
-                'id' => 10,
-                'name' => 'TRIENAL',
-                'description' => 'Esse é o componente 1'
-            ],
-            [
-                'id' => 11,
-                'name' => 'QUADRIENAL',
-                'description' => 'Esse é o componente 1'
-            ],
-            [
-                'id' => 12,
-                'name' => 'QUINQUENAL',
-                'description' => 'Esse é o componente 1'
-            ],
-            [
-                'id' => 13,
-                'name' => 'DECENAL',
-                'description' => 'Esse é o componente 1'
-            ],
-            [
-                'id' => 14,
-                'name' => 'OUTROS',
-                'description' => 'Esse é o componente 1'
-            ],
-        ];
+        $frequencia = Component::where('nome', 'frequencia')->get();
         return response()->json($frequencia);
     }
 
     public function material()
     {
-            
-            $material = [
-                [
-                    'id' => 1,
-                    'name' => 'KLUBEROIL 4 UH1-1500 N SPRAY SAM',
-                    'description' => 'Esse é o componente 1'
-                ],
-                [
-                    'id' => 3,
-                    'name' => 'OUTROS',
-                    'description' => 'Esse é o componente 1'
-                ],
-            ];
-    
-            return response()->json($material);
+
+        $material = ProdutoLub::all();
+
+        return response()->json($material);
     }
 
     public function nsf()
     {
-        $nsf = [
-            [
-                'id' => 1,
-                'name' => 'H1',
-                'description' => 'Esse é o componente 1'
-            ],
-            [
-                'id' => 2,
-                'name' => 'OUTROS',
-                'description' => 'Esse é o componente 1'
-            ],
-        ];
+        $nsf = Component::where('nome', 'nsf')->get();
 
         return response()->json($nsf);
     }
---
+++ app/Models/UnidadeMed.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UnidadeMed extends Model
+{
+    use HasFactory;
+
+    protected $table = 'unidade_medida';
+}
