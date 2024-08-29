880fb4295554a1e00e53b37845b7b18668bfd75b
simetria-studio <fecruz2022@gmail.com>
2024-08-29 16:06:29 -0300

    adw

--- app/Http/Controllers/api/PlanoLubController.php
+++ app/Http/Controllers/api/PlanoLubController.php
@@ -8,13 +8,14 @@ use App\Models\Linha;
 use App\Models\Plano;
 
 use App\Models\Cliente;
+use App\Models\Maquina;
 use App\Models\SubArea;
 use App\Models\PontoLub;
 use App\Models\EquipMaster;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+use App\Jobs\ProcessPlanoJob;
 use App\Models\AtvComponente;
-use App\Models\Maquina;
+use App\Http\Controllers\Controller;
 
 class PlanoLubController extends Controller
 {
@@ -22,122 +23,15 @@ class PlanoLubController extends Controller
 
     public function store(Request $request)
     {
-        try {
-            $data = $request->all();
+        $data = $request->all();
 
-            \Log::info($data);
-
-            foreach ($data as $planoData) {
-                // Salvar o plano de lubrificação
-                $lastPlano = Plano::orderBy('numero_plano', 'desc')->first(); // Encontre o último plano
-                $nextNumeroPlano = $lastPlano ? (int) $lastPlano->numero_plano + 1 : 1; // Adicione 1 ao último número do plano ou comece com 1 se não houver planos existentes
-                $existingPlano = Plano::where('codigo_mobile', $planoData['codigo_mobile'])->first();
-                $cliente = Cliente::where('razao_social', $planoData['cliente'])->first();
-                if (!$existingPlano) {
-                    $plano = Plano::create([
-                        'codigo_empresa' => '0001',
-                        'numero_plano' => '000' . str_pad($nextNumeroPlano, 3, '0', STR_PAD_LEFT),
-                        'codigo_unidade' => $cliente->codigo_cliente ?? ' ',
-                        'versao_plano' => ' ',
-                        'data_plano' => date("Y-m-d", strtotime($planoData['data_cadastro'])),
-                        'data_revisao' => date("Y-m-d", strtotime($planoData['data_revisao'])),
-                        'nome_supervisor' => ' ',
-                        'nome_lubrificador' => $planoData['responsavel_lubrificacao'] ?? ' ',
-                        'responsavel_kluber' => $planoData['responsavel_kluber'] ?? ' ',
-                        'ativo' => 'S',
-                        'codigo_mobile' => $planoData['codigo_mobile'] ?? ' ',
-                    ]);
-
-                    // Iterar sobre as áreas
-                    foreach ($planoData['areas'] as $area) {
-                        $novaArea = new Area();
-                        $novaArea->nome_area = $area['nome'];
-                        $novaArea->id_plano = $plano->id;
-                        $novaArea->ativo = 'S';
-                        $novaArea->save();
-
-                        // Iterar sobre as subáreas
-                        foreach ($area['subareas'] as $subarea) {
-                            $novaSubarea = new SubArea();
-                            $novaSubarea->nome_subarea = $subarea['nome'];
-                            $novaSubarea->id_area = $novaArea->id;
-                            $novaSubarea->ativo = 'S';
-                            $novaSubarea->save();
-
-                            // Iterar sobre as linhas
-                            foreach ($subarea['linhas'] as $linha) {
-                                $novaLinha = new Linha();
-                                $novaLinha->nome_linha = $linha['nome'];
-                                $novaLinha->id_subarea = $novaSubarea->id;
-                                $novaLinha->ativo = 'S';
-                                $novaLinha->save();
-
-
-                                foreach ($linha['tags_maquinas'] as $tagMaquina) {
-                                    $novaTagMaquina = new Maquina();
-                                    $novaTagMaquina->nome_maquina = $tagMaquina['maquina_nome'] ?? ' ';
-                                    $novaTagMaquina->id_linha = $novaLinha->id;
-                                    $novaTagMaquina->tag = $tagMaquina['tag_nome'] ?? ' ';
-                                    $novaTagMaquina->ativo = 'S';
-                                    $novaTagMaquina->save();
-
-                                    // Iterar sobre os conjuntos de equipamentos
-                                    foreach ($tagMaquina['conjuntos_equip'] as $conjuntoEquip) {
-                                        $novoConjuntoEquip = new EquipMaster();
-                                        $novoConjuntoEquip->codigo_empresa = '0001';
-                                        $novoConjuntoEquip->planta = $plano->codigo_unidade;
-                                        $novoConjuntoEquip->area = $novaArea->id;
-                                        $novoConjuntoEquip->subarea = $novaSubarea->id;
-                                        $novoConjuntoEquip->linha = $novaLinha->id;
-                                        $novoConjuntoEquip->maquina = $novaTagMaquina->id;
-                                        $novoConjuntoEquip->conjunto = $conjuntoEquip['conj_nome'];
-                                        $novoConjuntoEquip->equipamento = $conjuntoEquip['equi_nome'];
-                                        $novoConjuntoEquip->numero_plano = $plano->numero_plano;
-                                        $novoConjuntoEquip->qtde_pontos_total = '  ';
-                                        $novoConjuntoEquip->ativo = 'S';
-                                        $novoConjuntoEquip->save();
-                                        // Iterar sobre os pontos
-                                        $sequencial = 1; // Inicializa a variável contadora sequencial
-
-                                        foreach ($conjuntoEquip['pontos'] as $ponto) {
-                                            $componente = AtvComponente::create([
-                                                'codigo_empresa' => '0001',
-                                                'id_equipamento' => $novoConjuntoEquip->id,
-                                                'componente' => $ponto['component_codigo'],
-                                                'numero_plano' => $plano->numero_plano,
-                                                'numero_ponto' => $sequencial, // Use a variável sequencial como número de ponto
-                                                'qtde_pontos' => $ponto['qty_pontos'] ?? ' ',
-                                                'condicao_operacional' => $ponto['cond_op_codigo'] ?? ' ',
-                                                'descritivo_simplificado' => $ponto['atv_breve_name'] ?? ' ',
-                                                'descritivo_longo' => $ponto['atv_breve_name'] ?? ' ',
-                                                'frequencia' => $ponto['period_codigo'] ?? ' ',
-                                                'tempo_atividade' => $ponto['tempo_atv'] ?? ' ',
-                                                'qtde_pessoas' => $ponto['qty_pessoas'] ?? ' ',
-                                                'qtde_material' => $ponto['qty_material'] ?? ' ',
-                                                'unidade_medida' => $ponto['unidade_medida_codigo'] ?? ' ',
-                                                'material' => $ponto['lub_codigo'] ?? ' ',
-                                                'codigo_produto' => ' ',
-                                                'nsf' => '  ',
-                                                'data_ultimo_lancamento' => Carbon::now(),
-                                                'ativo' => 'S',
-                                            ]);
-
-                                            $sequencial++; // Incrementa a variável sequencial para o próximo número de ponto
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-
-            return response()->json('Plano(s) de lubrificação cadastrado(s) com sucesso!');
-        } catch (\Exception $e) {
-            // Handle the exception here
-            \Log::info($e->getMessage());
-            return response()->json('Ocorreu um erro ao processar a solicitação.', 500);
+        \Log::info('Plano de lubrificação recebido: ' . json_encode($data));
+        foreach ($data as $planoData) {
+            // Dispara o job para processamento assíncrono
+            ProcessPlanoJob::dispatch($planoData);
         }
+
+        return response()->json('Plano(s) de lubrificação sendo processado(s)!');
     }
 
 
---
+++ app/Jobs/ProcessPlanoJob.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Plano;
+use App\Models\Area;
+use App\Models\SubArea;
+use App\Models\Linha;
+use App\Models\Maquina;
+use App\Models\EquipMaster;
+use App\Models\AtvComponente;
+use App\Models\Cliente;
+use Carbon\Carbon;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class ProcessPlanoJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $planoData;
+
+    public function __construct($planoData)
+    {
+        $this->planoData = $planoData;
+    }
+
+    public function handle()
+    {
+     
+        try {
+            $planoData = $this->planoData;
+            $lastPlano = Plano::orderBy('numero_plano', 'desc')->first();
+            $nextNumeroPlano = $lastPlano ? (int) $lastPlano->numero_plano + 1 : 1;
+            $existingPlano = Plano::where('codigo_mobile', $planoData['codigo_mobile'])->first();
+            $cliente = Cliente::where('razao_social', $planoData['cliente'])->first();
+
+            if (!$existingPlano) {
+                $plano = Plano::create([
+                    'codigo_empresa' => '0001',
+                    'numero_plano' => '000' . str_pad($nextNumeroPlano, 3, '0', STR_PAD_LEFT),
+                    'codigo_unidade' => $cliente->codigo_cliente ?? ' ',
+                    'versao_plano' => ' ',
+                    'data_plano' => date("Y-m-d", strtotime($planoData['data_cadastro'])),
+                    'data_revisao' => date("Y-m-d", strtotime($planoData['data_revisao'])),
+                    'nome_supervisor' => ' ',
+                    'nome_lubrificador' => $planoData['responsavel_lubrificacao'] ?? ' ',
+                    'responsavel_kluber' => $planoData['responsavel_kluber'] ?? ' ',
+                    'ativo' => 'S',
+                    'codigo_mobile' => $planoData['codigo_mobile'] ?? ' ',
+                ]);
+
+                foreach ($planoData['areas'] as $area) {
+                    $novaArea = new Area();
+                    $novaArea->nome_area = $area['nome'];
+                    $novaArea->id_plano = $plano->id;
+                    $novaArea->ativo = 'S';
+                    $novaArea->save();
+
+                    foreach ($area['subareas'] as $subarea) {
+                        $novaSubarea = new SubArea();
+                        $novaSubarea->nome_subarea = $subarea['nome'];
+                        $novaSubarea->id_area = $novaArea->id;
+                        $novaSubarea->ativo = 'S';
+                        $novaSubarea->save();
+
+                        foreach ($subarea['linhas'] as $linha) {
+                            $novaLinha = new Linha();
+                            $novaLinha->nome_linha = $linha['nome'];
+                            $novaLinha->id_subarea = $novaSubarea->id;
+                            $novaLinha->ativo = 'S';
+                            $novaLinha->save();
+
+                            foreach ($linha['tags_maquinas'] as $tagMaquina) {
+                                $novaTagMaquina = new Maquina();
+                                $novaTagMaquina->nome_maquina = $tagMaquina['maquina_nome'] ?? ' ';
+                                $novaTagMaquina->id_linha = $novaLinha->id;
+                                $novaTagMaquina->tag = $tagMaquina['tag_nome'] ?? ' ';
+                                $novaTagMaquina->ativo = 'S';
+                                $novaTagMaquina->save();
+
+                                foreach ($tagMaquina['conjuntos_equip'] as $conjuntoEquip) {
+                                    $novoConjuntoEquip = new EquipMaster();
+                                    $novoConjuntoEquip->codigo_empresa = '0001';
+                                    $novoConjuntoEquip->planta = $plano->codigo_unidade;
+                                    $novoConjuntoEquip->area = $novaArea->id;
+                                    $novoConjuntoEquip->subarea = $novaSubarea->id;
+                                    $novoConjuntoEquip->linha = $novaLinha->id;
+                                    $novoConjuntoEquip->maquina = $novaTagMaquina->id;
+                                    $novoConjuntoEquip->conjunto = $conjuntoEquip['conj_nome'];
+                                    $novoConjuntoEquip->equipamento = $conjuntoEquip['equi_nome'];
+                                    $novoConjuntoEquip->numero_plano = $plano->numero_plano;
+                                    $novoConjuntoEquip->qtde_pontos_total = '  ';
+                                    $novoConjuntoEquip->ativo = 'S';
+                                    $novoConjuntoEquip->save();
+
+                                    $sequencial = 1;
+                                    foreach ($conjuntoEquip['pontos'] as $ponto) {
+                                        AtvComponente::create([
+                                            'codigo_empresa' => '0001',
+                                            'id_equipamento' => $novoConjuntoEquip->id,
+                                            'componente' => $ponto['component_codigo'],
+                                            'numero_plano' => $plano->numero_plano,
+                                            'numero_ponto' => $sequencial,
+                                            'qtde_pontos' => $ponto['qty_pontos'] ?? ' ',
+                                            'condicao_operacional' => $ponto['cond_op_codigo'] ?? ' ',
+                                            'descritivo_simplificado' => $ponto['atv_breve_name'] ?? ' ',
+                                            'descritivo_longo' => $ponto['atv_breve_name'] ?? ' ',
+                                            'frequencia' => $ponto['period_codigo'] ?? ' ',
+                                            'tempo_atividade' => $ponto['tempo_atv'] ?? ' ',
+                                            'qtde_pessoas' => $ponto['qty_pessoas'] ?? ' ',
+                                            'qtde_material' => $ponto['qty_material'] ?? ' ',
+                                            'unidade_medida' => $ponto['unidade_medida_codigo'] ?? ' ',
+                                            'material' => $ponto['lub_codigo'] ?? ' ',
+                                            'codigo_produto' => ' ',
+                                            'nsf' => '  ',
+                                            'data_ultimo_lancamento' => Carbon::now(),
+                                            'ativo' => 'S',
+                                        ]);
+
+                                        $sequencial++;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (\Exception $e) {
+            \Log::error($e->getMessage());
+            throw $e; // Para garantir que a fila detecte o erro e re-tente se configurado.
+        }
+    }
+}
+
